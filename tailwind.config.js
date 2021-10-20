e1596f16f622697cc98a6799b22e98ce3b384523
Taijul Islam <taijulislam.st9@gmail.com>
2021-10-20 23:54:36 +0600

    Complited class 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#FF8989",
+          200: "#FF8910",
+        },
+      },
+      fontFamily: {
+        customFont: ["Nunito"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
